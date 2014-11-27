24c4dad58115273ccb1acb2023cf1facaefc3f92
monkstone <martin_p@lineone.net>
2014-11-27 20:28:54 +0000

    replace erb with format

--- lib/jruby_art/creator.rb
+++ lib/jruby_art/creator.rb
@@ -1,44 +1,60 @@
+CLASS_BASIC = <<-CODE
+require 'jruby_art'
+
+class %s < Processing::App
+  def setup
+    size %s, %s
+  end
+
+  def draw
+
+  end
+end
+
+%s.new(title: '%s')
+CODE
+
+CLASS_MODE = <<-CODE
+require 'jruby_art'
+
+class %s < Processing::AppGL
+  def setup
+    size %s, %s, %s
+  end
+
+  def draw
+
+  end
+end
+
+%s.new(title: '%s')
+CODE
+
 module Processing
-  require 'erb'
   require_relative '../jruby_art/helpers/string'
+  # Write file to disk
   class SketchWriter
-    include ERB::Util
-
-    def initialize(template, param = {})
-      @name = param[:name]
-      @file_name = param[:file_name]
-      @title = param[:title]
-      @width = param.fetch(:width, 200)
-      @height = param.fetch(:height, 200)
-      @mode = param[:mode].upcase unless param[:mode].nil?
-      @template = template
-      save(@file_name)
+    attr_reader :file
+    def initialize(path)
+      @file = "#{File.dirname(path)}/#{path.underscore}.rb"
     end
 
-    def render
-      ERB.new(@template).result(binding)
-    end
-
-    def save(file)
+    def save(template)
       File.open(file, 'w+') do |f|
-        f.write(render)
+        f.write(template)
       end
     end
   end
 
   # An abstract class providing common methods for real creators
   class Creator
-
     ALL_DIGITS = /\A\d+\Z/
 
     def already_exist(path)
       new_file = "#{File.dirname(path)}/#{path.underscore}.rb"
-      if !File.exist?(path) && !File.exist?(new_file)
-        return
-      else
-        puts 'That file already exists!'
-        exit
-      end
+      return if !File.exist?(path) && !File.exist?(new_file)
+      puts 'That file already exists!'
+      exit
     end
 
     # Show the help/usage message for create.
@@ -47,74 +63,35 @@ module Processing
 
       Usage: k9 create <sketch_to_generate> <width> <height>
       Examples: k9 create my_app 800 600
-      k9 create my_app 800 600 
+                k9 create my_app 800 600 p3d
 
       USAGE
     end
-
   end
-
 
   # This class creates class wrapped sketches, with an optional render mode
   class ClassSketch < Creator
-
     def class_template
-%(
-require 'jruby_art'
-
-class <%=@name%> < Processing::App
-  def setup
-    size <%=@width%>, <%=@height%>
-  end
-
-  def draw
-
-  end
-end
-
-<%=@name%>.new(title: '<%=@title%>')
-)
+      format(CLASS_BASIC, @name, @width, @height, @name, @title)
     end
 
     def class_template_mode
-%(
-require 'jruby_art'
-
-class <%=@name%> < Processing::AppGL
-  def setup
-    size <%=@width%>, <%=@height%>, <%=@mode%>
-  end
-
-  def draw
-
-  end
-end
-
-<%=@name%>.new(title: '<%=@title%>')
-)
+      format(CLASS_MODE, @name, @width, @height, @mode, @name, @title)
     end
+
     # Create a bare blank sketch, given a path.
     def create!(path, args)
       return usage if /\?/ =~ path || /--help/ =~ path
-      main_file = File.basename(path, '.rb')
+      main_file = File.basename(path, '.rb') # allow uneeded extension input
       # Check to make sure that the main file doesn't exist already
       already_exist(path)
-      @param = {
-        name: main_file.camelize,
-        file_name: "#{File.dirname(path)}/#{path.underscore}.rb",
-        title: main_file.titleize,
-        width: args[0],
-        height: args[1],
-        mode: args[2]
-      }
-      @with_size = @param[:width] && @param[:width].match(ALL_DIGITS) &&
-        @param[:height] && @param[:height].match(ALL_DIGITS)
-      template = @param[:mode].nil? ? class_template : class_template_mode
-      sketch_writer(template, @param)
-    end
-    
-    def sketch_writer(template, param)
-      SketchWriter.new(template, param)
+      @name = main_file.camelize
+      writer = SketchWriter.new(main_file)
+      @title = main_file.titleize
+      @width, @height = args[0], args[1]
+      @mode = args[2].upcase unless args[2].nil?
+      template = @mode.nil? ? class_template : class_template_mode
+      writer.save(template)
     end
   end
 end
