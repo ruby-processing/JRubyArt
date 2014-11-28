ec3ac8f80c1ecc8a5ea75e1aa690aed3be09d404
monkstone <martin_p@lineone.net>
2014-11-28 07:42:08 +0000

    don't need no erb thank-you

--- lib/jruby_art/parse.rb
+++ lib/jruby_art/parse.rb
@@ -9,38 +9,45 @@ require_relative '../jruby_art/helpers/string'
 
 module Processing
   # For use with "bare" sketches that don't want to define a class or methods
-  SKETCH_TEMPLATE = <<-EOS
+  BARE_TEMPLATE = <<-EOS
   require 'jruby_art'
 
-  class Sketch < <%= mode %>
-    <% if has_methods %>
-    <%= source %>
-    <% else %>
-    def setup
-      <%= source %>
-      no_loop
-    end
-    <% end %>
+  class Sketch < %s
+    %s
   end
 
-  Sketch.new(title: <%= %q(title) %>)
+  Sketch.new(title: '%s')
+  EOS
+  
+  NAKED_TEMPLATE = <<-EOS
+  require 'jruby_art'
+
+  class Sketch < Processing::App
+
+    def setup
+      %s
+      no_loop
+    end
+  end
+
+  Sketch.new(title: '%s')
   EOS
 
   # This method is the common entry point to run a sketch, bare or complete.
   def self.load_and_run_sketch
     source = read_sketch_source
     has_sketch = !source.match(/^[^#]*< Processing::App/).nil?
-    has_methods = !source.match(/^[^#]*(def\s+setup|def\s+draw)/).nil?
-    default = source.match(/P(2|3)D/)
-    if has_sketch
-      load File.join(SKETCH_ROOT, SKETCH_PATH)
-    else
-      require 'erb'
-      title = File.basename(SKETCH_PATH).sub(/(\.rb)$/, '').titleize
+    has_methods = !source.match(/^[^#]*(def\s+setup|def\s+draw)/).nil?    
+    return load File.join(SKETCH_ROOT, SKETCH_PATH) if has_sketch
+    title = File.basename(SKETCH_PATH).sub(/(\.rb)$/, '').titleize
+    if has_methods
+      default = source.match(/P(2|3)D/)      
       mode = default ? 'Processing::App' : 'Processing::AppGL'
-      code = ERB.new(SKETCH_TEMPLATE).result(binding)
-      Object.class_eval code, SKETCH_PATH, -1
+      code = format(BARE_TEMPLATE, mode, source, title)
+    else     
+      code = format(NAKED_TEMPLATE, source, title)
     end
+    Object.class_eval code, SKETCH_PATH, -1
   end
 
   # Read in the sketch source code. Needs to work both online and offline.
