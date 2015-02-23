c7e5ed4b0720cdebf29f7b524fd6ef867eeb31ef
monkstone <martin_p@lineone.net>
2015-02-23 14:02:53 +0000

    add ruby library loader

--- lib/jruby_art/library_loader.rb
+++ lib/jruby_art/library_loader.rb
@@ -1,10 +1,12 @@
 # The LibraryLoader class add some logic for loading library jars
 class LibraryLoader
-  attr_reader :libraries_path, :loaded_libraries
+  attr_reader :libraries_path, :loaded_libraries, :sketch_root, :local
 
   def initialize
     @libraries_path = "#{ENV['HOME']}/.jruby_art/libraries"
     @loaded_libraries = []
+    @sketch_root = defined?(SKETCH_ROOT).nil? ? Dir.pwd : SKETCH_ROOT
+    @local = File.absolute_path("#{sketch_root}/library/")
   end
 
   # Load a list of Java libraries
@@ -13,6 +15,7 @@ class LibraryLoader
   # If a library is put into a 'library' folder next to the sketch it will
   # be used instead of the library that ships with Ruby-Processing.
   def load_libraries(*args)
+    local_ruby_lib(*args)
     local_java_lib(*args)
     installed_java_lib(*args)
   end
@@ -31,13 +34,22 @@ class LibraryLoader
   end
 
   def local_java_lib(*args)
-    library = "#{SKETCH_ROOT}/library/"
     args.each do |arg|
-      lib = File.join(library, arg.to_s)
+      next if loaded_libraries.include? arg
+      lib = File.join(local, arg.to_s)
       jar_files = File.join(lib, '**', '*.jar')
       Dir.glob(jar_files).each do |jar|
         require jar
       end
+      loaded_libraries << arg
+    end
+  end
+  
+  def local_ruby_lib(*args)
+    args.each do |arg|
+      ruby_file = File.join(local, format('%s.rb', arg))
+      next unless FileTest.exist? ruby_file
+      require ruby_file
       loaded_libraries << arg
     end
   end
