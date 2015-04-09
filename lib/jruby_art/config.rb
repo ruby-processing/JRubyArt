79ad5826e251c62544a56a23c9d859ee119e752e
monkstone <martin_p@lineone.net>
2015-04-09 16:11:57 +0100

    require 'yaml', rather than 'psych'

--- lib/jruby_art/config.rb
+++ lib/jruby_art/config.rb
@@ -1,13 +1,11 @@
-require 'psych'
+require 'yaml'
 # The Processing module is a wrapper for JRubyArt
 # Author:: Martin Prout (extends / re-implements ruby-processing)
 module Processing
   unless defined? RP_CONFIG
     begin
       CONFIG_FILE_PATH = File.expand_path('~/.jruby_art/config.yml')
-      File.open(CONFIG_FILE_PATH, 'r') do |yaml|
-        RP_CONFIG = Psych.load(yaml)
-      end
+      RP_CONFIG = YAML.load_file(yaml)
     rescue
       warn('WARNING: you need to set PROCESSING_ROOT in ~/.jruby_art/config.yml')
     end
