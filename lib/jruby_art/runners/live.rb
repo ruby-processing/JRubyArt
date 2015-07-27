bad6f5d140e98c14d616014481967f44b15a3b98
monkstone <martin_p@lineone.net>
2015-07-27 22:14:34 +0100

    use IRB for 'live' if 'pry' is not installed

--- lib/jruby_art/runners/live.rb
+++ lib/jruby_art/runners/live.rb
@@ -1,10 +1,32 @@
-# An pry shell for live coding.
+# An IRB/pry shell for live coding.
 # Will start with your sketch.
-
 require_relative 'base'
 Processing.load_and_run_sketch
 
-ARGV.clear # So that pry doesn't try to load them as files.
+if Gem::Specification.find_all_by_name('pry').any?
+  require 'pry'
+  $app.pry
+else
+  require 'irb'
 
-require 'pry'
-$app.pry
+  module IRB # :nodoc:
+    def self.start_session(binding)
+      unless @__initialized
+        args = ARGV
+        ARGV.replace(ARGV.dup)
+        IRB.setup(nil)
+        ARGV.replace(args)
+        @__initialized = true
+      end
+      workspace = WorkSpace.new(binding)
+      irb = Irb.new(workspace)
+      @CONF[:IRB_RC].call(irb.context) if @CONF[:IRB_RC]
+      @CONF[:MAIN_CONTEXT] = irb.context
+      catch(:IRB_EXIT) do
+        irb.eval_input
+      end
+    end
+  end
+end
+
+IRB.start_session($app)
