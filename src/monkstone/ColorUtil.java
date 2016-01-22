40518e639f4aec3231bef5fe22eee89552717b2c
monkstone <martin_p@lineone.net>
2016-01-22 11:42:02 +0000

    package monkstone went walkabout

--- src/monkstone/ColorUtil.java
+++ src/monkstone/ColorUtil.java
@@ -1,6 +1,6 @@
 /**
  * This utility allows JRubyArt users to use the processing.org color method 
- * in their sketches Copyright (C) 2015 Martin Prout. 
+ * in their sketches Copyright (C) 2015-16 Martin Prout. 
  * This utility is free software; you can redistribute it and/or modify 
  * it under the terms of the GNU Lesser General Public License as published by 
  * the Free Software Foundation; either version 2.1 of the License, or (at 
@@ -8,7 +8,8 @@
  * 
  * Obtain a copy of the license at http://www.gnu.org/licenses/lgpl-2.1.html
  */
-package monkstone;
+ 
+ package monkstone;
 
 /**
  *
