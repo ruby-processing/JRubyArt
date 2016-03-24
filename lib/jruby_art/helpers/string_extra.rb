4873bef9858f6ae98184ed3f53d0aa2551f7e499
monkstone <martin_p@lineone.net>
2016-03-24 20:16:31 +0000

    rubocop suggestions

--- lib/jruby_art/helpers/string_extra.rb
+++ lib/jruby_art/helpers/string_extra.rb
@@ -3,13 +3,14 @@
 
 require 'forwardable'
 
+# String utility for creating titles and class-names
 class StringExtra
   extend Forwardable
-  def_delegators :@str, :upcase, :capitalize, :length, :downcase, :gsub 
+  def_delegators :@str, :upcase, :capitalize, :length, :downcase, :gsub, :tr
   def initialize(str)
     @str = str
   end
-  
+
   def titleize
     gsub(/::/, '/')
       .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
@@ -17,30 +18,28 @@ class StringExtra
       .tr('-', '_')
       .downcase
       .gsub(/_id$/, '')
-      .gsub(/_/, ' ').capitalize
+      .tr('_', ' ').capitalize
       .gsub(/\b([a-z])/) { Regexp.last_match[1].capitalize }
   end
 
   def humanize
-    gsub(/_id$/, '').gsub(/_/, ' ').capitalize
+    gsub(/_id$/, '').tr(/_/, ' ').capitalize
   end
 
   def underscore
     gsub(/::/, '/')
       .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
       .gsub(/([a-z\d])([A-Z])/, '\1_\2')
-      .tr('-', '_')
+      .sub('-', '_')
       .downcase
   end
-  
+
   def camelize(first_letter_in_uppercase = true)
     if first_letter_in_uppercase
       @str.gsub(%r{/(.?)}) { '::' + Regexp.last_match[1].upcase }
-        .gsub(/(^|_)(.)/) { Regexp.last_match[2].upcase }
+          .gsub(/(^|_)(.)/) { Regexp.last_match[2].upcase }
     else
       @str[0] + camelize[1..-1]
     end
   end
 end
-
-
