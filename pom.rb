db7e8fb4169654b53c61c951b4dff23c749e683a
monkstone <martin_p@lineone.net>
2016-05-03 19:37:34 +0100

    jar version

--- pom.rb
+++ pom.rb
@@ -5,7 +5,7 @@ require 'fileutils'
 project 'rp5extras', 'https://github.com/ruby-processing/JRubyArt' do
 
   model_version '4.0.0'
-  id 'ruby-processing:rp5extras', '1.0.5'
+  id 'ruby-processing:rp5extras', '1.0.7'
   packaging 'jar'
 
   description 'rp5extras for JRubyArt'
