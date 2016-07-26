b5f01440ed2d3da2ca6617712c2d6c4225ee63f7
monkstone <mamba2928@yahoo.co.uk>
2016-07-26 11:42:11 +0100

    getting better

--- lib/jruby_art/creators/sketch_writer.rb
+++ lib/jruby_art/creators/sketch_writer.rb
@@ -1,22 +1,45 @@
 # frozen_string_literal: false
-# Sketch class
+# SketchParamters class
+class SketchParameters
+  attr_reader :name, :args
+  def initialize(name:, args:)
+    @name = name
+    @args = args
+  end
+
+  def class_name
+    name.split('_').collect(&:capitalize).join
+  end
+
+  def sketch_title
+    human = name.split('_').collect(&:capitalize).join(' ')
+    format("sketch_title '%s'", human)
+  end
+
+  def sketch_size
+    mode = args.length == 3 ? format(', %s', args[2].upcase) : ''
+    return 'size 200, 200' if args.empty?
+    format('size %d, %d%s', args[0].to_i, args[1].to_i, mode)
+  end
+end
+
+# The file writer knows different types
 class SketchWriter
-  attr_reader :file, :args, :sketch, :name
+  attr_reader :file, :sketch, :param
 
   def initialize(path, args)
-    @args = args
-    @name = path
+    @param = SketchParameters.new(name: path, args: args)
     @file = format('%s/%s.rb', File.dirname(path), path)
   end
 
   def create!(type)
     case type
     when /bare/
-      @sketch = Sketch.new.bare(name, args)
+      @sketch = Sketch.new.bare(param)
     when /class/
-      @sketch = Sketch.new.class_wrapped(name, args)
+      @sketch = Sketch.new.class_wrapped(param)
     when /emacs/
-      @sketch = Sketch.new.emacs(name, args)
+      @sketch = Sketch.new.emacs(param)
     end
     save(sketch)
   end
@@ -28,60 +51,52 @@ end
 
 # The sketch class creates an array of formatted sketch lines
 class Sketch
-  def bare(name, args)
-    mode = args.length == 3 ? format(', %s', args[2].upcase) : ''
-    size = args.empty? ? 'size 200, 200' : format('size %d, %d%s', args[0].to_i, args[1].to_i, mode)
+  def bare(param)
     lines = []
-    lines.concat method_lines('settings', size, '')
-    sketch_title = name.split('_').collect(&:capitalize).join(' ')
-    lines.concat method_lines('setup', format("sketch_title '%s'", sketch_title), '')
-    lines.concat last_method('draw', '', '')
+    lines.concat method_lines('settings', param.sketch_size, '')
+    lines.concat method_lines('setup', param.sketch_title, '')
+    lines.concat method_lines('draw', '', '')
   end
 
-  def wrapped(name, args)
-    mode = args.length == 3 ? format(', %s', args[2].upcase) : ''
-    size = args.empty? ? 'size 200, 200' : format('size %d, %d%s', args[0].to_i, args[1].to_i, mode)
+  def wrapped(param)
     lines = []
-    class_name = name.split('_').collect(&:capitalize).join
+    class_name = param.name.split('_').collect(&:capitalize).join
     lines << format('class %s < Processing::App', class_name)
-    lines.concat method_lines('settings', size, '  ')
-    sketch_title = name.split('_').collect(&:capitalize).join(' ')
-    lines.concat method_lines('setup', format("sketch_title '%s'", sketch_title), '  ')
-    lines.concat last_method('draw', '', '  ')
+    lines.concat method_lines('settings', param.sketch_size, '  ')
+    lines.concat method_lines('setup', param.sketch_title, '  ')
+    lines.concat method_lines('draw', '', '  ')
     lines << 'end'
   end
 
-  def class_wrapped(name, args)
-    lines = []
-    lines << '# frozen_string_literal: false'
-    lines << ''
-    lines.concat wrapped(name, args)
+  def class_wrapped(param)
+    lines = [
+      '# frozen_string_literal: false',
+      ''
+    ]
+    lines.concat wrapped(param)
   end
 
-  def emacs(name, args)
-    lines = []
-    lines << '# frozen_string_literal: false'
-    lines << "require 'jruby_art'"
-    lines << "require 'jruby_art/app'"
+  def emacs(param)
+    lines = [
+      '# frozen_string_literal: false',
+      "require 'jruby_art'",
+      "require 'jruby_art/app'",
+      '',
+      'Processing::App::SKETCH_PATH = __FILE__.freeze',
+      ''
+    ]
+    lines.concat wrapped(param)
     lines << ''
-    lines << 'Processing::App::SKETCH_PATH = __FILE__.freeze'
-    lines << ''
-    class_name = name.split('_').collect(&:capitalize).join
-    lines.concat wrapped(name, args)
-    lines << ''
-    lines << format('%s.new unless defined? $app', class_name)
+    lines << format('%s.new unless defined? $app', param.class_name)
   end
 
   private
 
-  def last_method(name, content, indent)
+  def method_lines(name, content, indent)
     one = format('%sdef %s', indent, name)
     two = content.empty? ? '' : format('  %s%s', indent, content)
     three = format('%send', indent)
-    [one, two, three]
-  end
-
-  def method_lines(name, content, indent)
-    last_method(name, content, indent) << ''
+    return [one, two, three] if /draw/ =~ name
+    [one, two, three, '']
   end
 end
--- test/create_test.rb
+++ test/create_test.rb
@@ -6,52 +6,15 @@ require 'minitest/pride'
 require_relative '../lib/jruby_art/creators/sketch_writer'
 
 EMACS = <<~CODE
-  # frozen_string_literal: false
-  require 'jruby_art'
-  require 'jruby_art/app'
+# frozen_string_literal: false
+require 'jruby_art'
+require 'jruby_art/app'
 
-  Processing::App::SKETCH_PATH = __FILE__.freeze
+Processing::App::SKETCH_PATH = __FILE__.freeze
 
-  class FredSketch < Processing::App
-    def settings
-      size 200, 200
-    end
-
-    def setup
-      sketch_title 'Fred Sketch'
-    end
-
-    def draw
-
-    end
-  end
-
-  FredSketch.new unless defined? $app
-
-CODE
-
-CLASS_SKETCH = <<~CODE
-  # frozen_string_literal: false
-  
-  class FredSketch < Processing::App
-    def settings
-      size 200, 200
-    end
-
-    def setup
-      sketch_title 'Fred Sketch'
-    end
-
-    def draw
-
-    end
-  end
-
-CODE
-
-BARE = <<~CODE
+class FredSketch < Processing::App
   def settings
-    size 200, 200, P2D
+    size 200, 200
   end
 
   def setup
@@ -61,37 +24,70 @@ BARE = <<~CODE
   def draw
 
   end
+end
+
+FredSketch.new unless defined? $app
+
+CODE
+
+CLASS_SKETCH = <<~CODE
+# frozen_string_literal: false
+
+class FredSketch < Processing::App
+  def settings
+    size 200, 200
+  end
+
+  def setup
+    sketch_title 'Fred Sketch'
+  end
+
+  def draw
+
+  end
+end
+
+CODE
+
+BARE = <<~CODE
+def settings
+  size 200, 200, P2D
+end
+
+def setup
+  sketch_title 'Fred Sketch'
+end
+
+def draw
+
+end
 
 CODE
 
 class SketchWriterTest < Minitest::Test
-
   def test_bare
-    name = 'fred_sketch'
-    size = %w(200 200 p2d)
+    param = SketchParameters.new(name: 'fred_sketch', args: %w(200 200 p2d))
     result = BARE.split(/\n/, -1)
-    sketch = ::Sketch.new.bare(name, size)
+    sketch = Sketch.new.bare(param)
     sketch.each_with_index do |line, i|
       assert_equal result[i], line
     end
   end
-
+  
   def test_class
-    name = 'fred_sketch'
-    size = %w(200 200)
+    param = SketchParameters.new(name: 'fred_sketch', args: %w(200 200))
     result = CLASS_SKETCH.split(/\n/, -1)
-    class_lines = ::Sketch.new.class_wrapped(name, size)
+    class_lines = Sketch.new.class_wrapped(param)
     class_lines.each_with_index do |line, i|
       assert_equal result[i], line
     end
   end
-
+  
   def test_emacs
-    name = 'fred_sketch'
-    size = %w(200 200)
+    param = SketchParameters.new(name: 'fred_sketch', args: %w(200 200))
     result = EMACS.split(/\n/, -1)
-    class_lines = ::Sketch.new.emacs(name, size)
-        class_lines.each_with_index do |line, i|
+    class_lines = Sketch.new.emacs(param)
+    class_lines.each_with_index do |line, i|
       assert_equal result[i], line
     end
   end
