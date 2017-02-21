a258a7b446ee70d8bd75f8d7621ffc9644e8650b
monkstone <mamba2928@yahoo.co.uk>
2017-02-21 14:17:13 +0000

    going all OO

--- lib/jruby_art/launcher.rb
+++ lib/jruby_art/launcher.rb
@@ -5,11 +5,13 @@ module Processing
   # The command class check for configuration and options, before creating and
   # executing the jruby (or java) command to run the sketch
   class Launcher
-    attr_reader :runner, :args, :filename
+    attr_reader :command
     def initialize(runner:, args:, filename:)
-      @runner = runner
-      @args = args
-      @filename = filename
+      @command = if Processing::RP_CONFIG.fetch('JRUBY', true)
+                   JRubyCommand.new(runner, filename, args)
+                 else
+                   JavaCommand.new(runner, filename, args)
+                 end
     end
 
     # Trade in this Ruby instance for a JRuby instance, loading in a starter
@@ -18,23 +20,40 @@ module Processing
     # of our vendored one. Note the use of jruby-complete might make using
     # other gems in your sketches hard (but not impossible)....
     def cmd
-      cmda = jruby_command
+      cmda = command.cmd
       begin
         exec(*cmda)
         # exec replaces the Ruby process with the JRuby one.
       rescue Java::JavaLang::ClassNotFoundException
       end
     end
+  end
+end
 
-    private
+# Wrap creation of java command string as a class
+class JavaCommand
+  MAIN = 'org.jruby.Main'.freeze
+  attr_reader :runner, :args, :filename, :opts, :complete
+  def initialize(runner, args, filename)
+    @runner, @args, @filename = runner, args, filename
+    @complete = JRubyComplete.complete
+    @opts = JavaOpts.new.opts
+  end
 
-    # avoiding multiline ternary etc
-    def jruby_command
-      installed = Processing::RP_CONFIG.fetch('JRUBY', true)
-      opts = installed ? JRubyOpts.new.opts : JavaOpts.new.opts
-      return ['jruby', opts, runner, filename, args].flatten if installed
-      complete = JRubyComplete.complete
-      ['java', opts, '-cp', complete, 'org.jruby.Main', runner, filename, args].flatten
-    end
+  def cmd
+    ['java', opts, '-cp', complete, MAIN, runner, filename, args].flatten
+  end
+end
+
+# Wrap creation of jruby command string as a class
+class JRubyCommand
+  attr_reader :runner, :args, :filename, :opts
+  def initialize(runner, args, filename)
+    @runner, @args, @filename = runner, args, filename
+    @opts = JRubyOpts.new.opts
+  end
+
+  def cmd
+    ['jruby', opts, runner, filename, args].flatten if installed
   end
 end
