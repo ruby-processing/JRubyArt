bbf19dccd4dd401e8b9d74145a271bdbc2839f5f
monkstone <mamba2928@yahoo.co.uk>
2017-09-06 16:06:08 +0100

    bump copyright date

--- src/monkstone/vecmath/vec2/Vec2.java
+++ src/monkstone/vecmath/vec2/Vec2.java
@@ -1,7 +1,7 @@
 package monkstone.vecmath.vec2;
 
 /* 
-* Copyright (c) 2015-16 Martin Prout
+* Copyright (c) 2015-17 Martin Prout
 * 
 * This library is free software; you can redistribute it and/or
 * modify it under the terms of the GNU Lesser General Public
