ac71b071eab84dbf5591ca6771ef67b21bedcffd
monkstone <mamba2928@yahoo.co.uk>
2017-09-06 16:18:22 +0100

    update copyright date

--- src/monkstone/slider/CustomHorizontalSlider.java
+++ src/monkstone/slider/CustomHorizontalSlider.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2016 Martin Prout
+ * Copyright (c) 2016-17 Martin Prout
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
