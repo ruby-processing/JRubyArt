03ff013b018a27450df25c9818704abeabff5eb5
monkstone <mamba2928@yahoo.co.uk>
2017-09-07 18:58:55 +0100

    config

--- lib/jruby_art/config.rb
+++ lib/jruby_art/config.rb
@@ -5,7 +5,7 @@ require 'yaml'
 # The wrapper module
 module Processing
   unless defined? RP_CONFIG
-    config_path = '~/.jruby_art/config.yml'
+    config_path = "ENV['HOME']/.jruby_art/config.yml"
     begin
       CONFIG_FILE_PATH = File.expand_path(config_path)
       RP_CONFIG = YAML.load_file(CONFIG_FILE_PATH)
