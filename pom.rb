7debe499f58e805eb7f92075a6b64316b21e0d25
monkstone <mamba2928@yahoo.co.uk>
2018-02-22 06:19:01 +0000

    Bump version

--- pom.rb
+++ pom.rb
@@ -2,7 +2,7 @@ require 'fileutils'
 project 'rp5extras', 'https://github.com/ruby-processing/JRubyArt' do
 
   model_version '4.0.0'
-  id 'ruby-processing:rp5extras', '1.4.5'
+  id 'ruby-processing:rp5extras', '1.4.6'
   packaging 'jar'
 
   description 'rp5extras for JRubyArt'
@@ -32,7 +32,7 @@ project 'rp5extras', 'https://github.com/ruby-processing/JRubyArt' do
     'jruby.api' => 'http://jruby.org/apidocs/'
   )
 
-  pom 'org.jruby:jruby:9.1.13.0'
+  pom 'org.jruby:jruby:9.1.16.0'
   jar 'org.processing:core:3.3.6'
   jar 'org.processing:video:3.0.2'
 
