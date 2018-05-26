3bb89d19bcac68f543beed098e8f1d4a0072cf08
monkstone <mamba2928@yahoo.co.uk>
2018-05-26 20:03:24 +0100

    Use match? ruby-2.4+

--- lib/jruby_art/native_folder.rb
+++ lib/jruby_art/native_folder.rb
@@ -4,8 +4,8 @@ require 'rbconfig'
 class NativeFolder
   attr_reader :os, :bit
 
-  WIN_FORMAT = 'windows%d'.freeze
-  LINUX_FORMAT = 'linux%d'.freeze
+  WIN_FORMAT = 'windows%s'.freeze
+  LINUX_FORMAT = 'linux%s'.freeze
   WIN_PATTERNS = [
     /bccwin/i,
     /cygwin/i,
@@ -17,18 +17,18 @@ class NativeFolder
 
   def initialize
     @os = RbConfig::CONFIG['host_os'].downcase
-    @bit = java.lang.System.get_property('os.arch') =~ /64/ ? 64 : 32
+    @bit = java.lang.System.get_property('os.arch')
   end
 
   def name
     return 'macosx' if os =~ /darwin/ || os =~ /mac/
-    return format(WIN_FORMAT, bit) if WIN_PATTERNS.any? { |pat| pat =~ os }
-    return format(LINUX_FORMAT, bit) if os =~ /linux/
+    return format(WIN_FORMAT, bit) if WIN_PATTERNS.any? { |pat| pat.match?(os) }
+    return format(LINUX_FORMAT, bit) if /linux/.match?(os)
   end
 
   def extension
     return '*.so' if os =~ /linux/
-    return '*.dll' if WIN_PATTERNS.any? { |pat| pat =~ os }
+    return '*.dll' if WIN_PATTERNS.any? { |pat| pat.match?(os) }
     '*.dylib' # MacOS
   end
 end
