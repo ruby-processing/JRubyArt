4fa612ea076d7c678554f16b2a0002bbf981be3f
Martin Prout <mamba2928@yahoo.co.uk>
2019-01-03 13:28:44 +0000

    bum copyright date

--- src/monkstone/arcball/Constrain.java
+++ src/monkstone/arcball/Constrain.java
@@ -1,18 +1,18 @@
-/* 
- * Copyright (c) 2015-17 Martin Prout
- * 
+/*
+ * Copyright (c) 2015-19 Martin Prout
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * http://creativecommons.org/licenses/LGPL/2.1/
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -29,7 +29,7 @@ public enum Constrain {
     /**
      * Used to constrain arc-ball rotation about X axis
      */
-    
+
     XAXIS(0),
     /**
      * Used to constrain arc-ball rotation about Y axis
