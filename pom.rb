022bd687cfdd74aa21876c09476d6cdf1759e0eb
monkstone <mamba2928@yahoo.co.uk>
2019-08-15 14:59:44 +0100

    jdeps plugin

--- pom.rb
+++ pom.rb
@@ -15,51 +15,56 @@ project 'jruby_art', 'https://github.com/ruby-processing/JRubyArt' do
   issue_management 'https://github.com/ruby-processing/JRubyArt/issues', 'Github'
 
   source_control( :url => 'https://github.com/ruby-processing/JRubyArt',
-    :connection => 'scm:git:git://github.com/ruby-processing/JRubyArt.git',
-    :developer_connection => 'scm:git:git@github.com/ruby-processing/JRubyArt.git' )
+                  :connection => 'scm:git:git://github.com/ruby-processing/JRubyArt.git',
+                  :developer_connection => 'scm:git:git@github.com/ruby-processing/JRubyArt.git' )
 
-    properties(
-      'processing.api' => 'http://processing.github.io/processing-javadocs/core/',
-      'polyglot.dump.pom' => 'pom.xml',
-      'project.build.sourceEncoding' => 'UTF-8',
-      'source.directory' => 'src',
-      'jogl.version' => '2.3.2',
-      'jruby.api' => 'http://jruby.org/apidocs/'
-    )
+  properties( 'processing.api' => 'http://processing.github.io/processing-javadocs/core/',
+              'source.directory' => 'src',
+              'polyglot.dump.pom' => 'pom.xml',
+              'project.build.sourceEncoding' => 'UTF-8',
+              'jogl.version' => '2.3.2',
+              'jruby.api' => 'http://jruby.org/apidocs/' )
 
-    pom 'org.jruby:jruby:9.2.8.0'
-    jar 'org.jogamp.jogl:jogl-all:${jogl.version}'
-    jar 'org.jogamp.gluegen:gluegen-rt-main:${jogl.version}'
-    jar 'org.processing:video:3.0.2'
+  pom 'org.jruby:jruby:9.2.8.0'
+  jar 'org.jogamp.jogl:jogl-all:${jogl.version}'
+  jar 'org.jogamp.gluegen:gluegen-rt-main:${jogl.version}'
+  jar 'org.processing:video:3.0.2'
 
-    overrides do
-      plugin :resources, '2.7'
-      plugin :dependency, '2.8'
-      plugin( :compiler, '3.8.0', 'release' => '11' )
-      plugin(
-        :javadoc,
-        '2.10.4',
-        'detectOfflineLinks' =>  'false',
-        'links' => [ '${processing.api}', '${jruby.api}' ]
-      )
-      plugin(
-        :jar, '3.1.0',
-        'archive' => {
-          'manifestEntries' => { 'Class-Path' => 'gluegen-rt.jar jog-all.jar' }
-        }
-      )
+  overrides do
+    plugin :resources, '2.7'
+    plugin :dependency, '2.8'
+    plugin( :compiler, '3.8.0',
+            'release' =>  '11' )
+    plugin( :javadoc, '2.10.4',
+            'detectOfflineLinks' =>  'false',
+            'links' => [ '${processing.api}',
+                         '${jruby.api}' ] )
+    plugin( :jar, '3.1.0',
+            'archive' => {
+              'manifestEntries' => {
+                'Class-Path' =>  'gluegen-rt.jar jog-all.jar'
+              }
+            } )
+    plugin :jdeps, '3.0.0' do
+      execute_goals 'jdkinternals', 'test-jdkinternals'
+    end
+    
+  end
+
+
+  build do
+
+    resource do
+      directory '${source.directory}/main/java'
+      includes '**/**/*.glsl', '**/*.jnilib'
+      excludes '**/**/*.java'
     end
 
-
-    build do
-      resource do
-        directory '${source.directory}/main/java'
-        includes '**/**/*.glsl', '**/*.jnilib'
-        excludes '**/**/*.java'
-      end
-      resource do
-        directory '${source.directory}/main/resources'
-        includes '**/*.png', '*.txt'
-      end
+    resource do
+      directory '${source.directory}/main/resources'
+      includes '**/*.png', '*.txt'
+      excludes
     end
   end
+
+end
